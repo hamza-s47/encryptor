3bee054ff1273a21911ed6af3c173c1704786e54
Hamza Siddiqui <hamza.siddiqui4747@gmail.com>
2024-10-08 11:03:41 +0500

    .

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from controllers.controller import encrypt_text, decrypt_text, encrypt_file, decrypt_file
+from .controllers.controller import encrypt_text, decrypt_text, encrypt_file, decrypt_file
 
 abc= encrypt_text("My name is Hamza")
 
-print(type(b'gAAAAABnBCGnl4nVY9bK5Fkm8Gb_Yod5vT5ySvnICtBgXMAPQw72P_R54uDaExXe1rLMAdG-ysfJBacRicJPkOEB_VgC5nIyaQfDdS7gz4pjZAnxXtgY3Kc='))
+# abc = b'gAAAAABnBCGnl4nVY9bK5Fkm8Gb_Yod5vT5ySvnICtBgXMAPQw72P_R54uDaExXe1rLMAdG-ysfJBacRicJPkOEB_VgC5nIyaQfDdS7gz4pjZAnxXtgY3Kc='
 
-# print(decrypt_text(b'gAAAAABnBCGnl4nVY9bK5Fkm8Gb_Yod5vT5ySvnICtBgXMAPQw72P_R54uDaExXe1rLMAdG-ysfJBacRicJPkOEB_VgC5nIyaQfDdS7gz4pjZAnxXtgY3Kc='))
+print(decrypt_text(abc))
