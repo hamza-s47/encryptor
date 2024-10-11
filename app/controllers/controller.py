a37c7aacc79840e3e89f87a010ccc28a6376724c
Hamza Siddiqui <hamza.siddiqui4747@gmail.com>
2024-10-11 18:58:16 +0500

    .

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -35,16 +35,3 @@ def decrypt_file(encrypted_file_path, key):
     decrypted_data = cipher.decrypt(encrypted_data)
     with open(encrypted_file_path.replace('.encrypted', ''), 'wb') as file:
         file.write(decrypted_data)
-
-# Example usage
-# if __name__ == "__main__":
-#     # Generate a key and save it (important!)
-#     key = Fernet.generate_key()
-#     with open('secret.key', 'wb') as key_file:
-#         key_file.write(key)
-
-#     # Encrypt a file
-#     encrypt_file('example.txt', key)
-
-#     # Decrypt the file
-#     decrypt_file('example.txt.encrypted', key)
