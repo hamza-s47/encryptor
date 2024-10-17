339ebab228d2c35c69de351b8cf087db51a2ff7e
Hamza Siddiqui <hamza.siddiqui4747@gmail.com>
2024-10-17 19:00:16 +0500

    .

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,37 +1,54 @@
 from cryptography.fernet import Fernet
 
+class EncryptorData:
+    def __init__(self) -> None:
+        self.__key = None
 
-# Encrypt text
-def encrypt_text(plain_text):
-    # Generate a key
-    key = Fernet.generate_key()
-    cipher = Fernet(key)
+    def __generate_key(self):
+        self.__key = Fernet.generate_key()
+    
+    @property
+    def my_key(self):
+        if self.__key is None:
+            self.__generate_key()
+            return self.__key
 
-    data = {
-        "encrypted_text":cipher.encrypt(plain_text.encode()),
-        "key":key
-    }
-    return data
+class TextEncryptor(EncryptorData):
+    
+    def encrypt_text(self, text):
+        key = self.my_key
+        cipher = Fernet(key)
+        encrypted_text = cipher.encrypt(text.encode())
+        data = {
+            "encrypted_text":encrypted_text,
+            "private_key":key
+        }
+        return data
+    
+    def decrypt_text(self, encrypted_text, key):
+        cipher = Fernet(key)
+        return cipher.decrypt(encrypted_text).decode()  
+    
+class FileEncryptor(EncryptorData):
 
-# Decrypt text
-def decrypt_text(encrypted_text, ktd):
-    f = Fernet(ktd)
-    return f.decrypt(encrypted_text).decode()
+    def encrypt_file(self, file_path):
+        """Encrypt a file."""
+        if self.key is None:
+            raise ValueError("Key is not set. Please generate or provide a key.")
+        cipher = Fernet(self.key)
+        with open(file_path, 'rb') as file:
+            file_data = file.read()
+        encrypted_data = cipher.encrypt(file_data)
+        with open(file_path + '.encrypted', 'wb') as file:
+            file.write(encrypted_data)
 
-# Encrypt file
-def encrypt_file(file_path, key):
-    cipher = Fernet(key)
-    with open(file_path, 'rb') as file:
-        file_data = file.read()
-    encrypted_data = cipher.encrypt(file_data)
-    with open(file_path + '.encrypted', 'wb') as file:
-        file.write(encrypted_data)
-
-# Decrypt file
-def decrypt_file(encrypted_file_path, key):
-    cipher = Fernet(key)
-    with open(encrypted_file_path, 'rb') as file:
-        encrypted_data = file.read()
-    decrypted_data = cipher.decrypt(encrypted_data)
-    with open(encrypted_file_path.replace('.encrypted', ''), 'wb') as file:
-        file.write(decrypted_data)
+    def decrypt_file(self, encrypted_file_path):
+        """Decrypt a file."""
+        if self.key is None:
+            raise ValueError("Key is not set. Please generate or provide a key.")
+        cipher = Fernet(self.key)
+        with open(encrypted_file_path, 'rb') as file:
+            encrypted_data = file.read()
+        decrypted_data = cipher.decrypt(encrypted_data)
+        with open(encrypted_file_path.replace('.encrypted', ''), 'wb') as file:
+            file.write(decrypted_data)
--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template
-from .controllers.controller import encrypt_text, decrypt_text, encrypt_file, decrypt_file
+from .controllers.controller import TextEncryptor
 
 app = Flask(__name__)
 
-# abc= encrypt_text("I love automation")
-# print(abc)
-
-print(decrypt_text(b'gAAAAABnBS-0pL0ltnIfMy1GgIVX3AzaB0CeGnij-dJ87b8zXgHn4raADsrQkei2w4pcS4Xe-sZtuZ0HcyLiyLJ-xA_tOPj1-v3zC3dcaDyorhX_NQ2xPpM=', b'covqtWq7K1S6zDKDEDSfBWyJqPjw2POiuuYAQ3g_EG8='))
 
 @app.route('/', methods=['GET'])
 def root_route():
